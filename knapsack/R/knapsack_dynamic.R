7965f4f5d6ea3e6dcfb3f4da25fd9ef177adf2c7
carsv733 <carsv733@student.liu.se>
2015-10-02 14:29:24 +0200

    mmm

--- knapsack/R/knapsack_dynamic.R
+++ knapsack/R/knapsack_dynamic.R
@@ -1,3 +1,4 @@
+set.seed(42)
 n <- 2000
 knapsack_objects <-
   data.frame(
@@ -5,23 +6,21 @@ knapsack_objects <-
     v=runif(n = n, 0, 10000))
 
 knapsack_dynamic <- function(x,W){
+ 
   n <- length(x$v)
-  weight <- 0
-  prevWeight <-0
-  
   m <- matrix(rep(0,(n+1)*(W+1)), n+1, W+1)
- #m <- matrix(rep(0,length(x$w))*length(x$v), length(x$w), length(x$v))
-  
+  elements <- numeric()
   for (i in 1:n) { #hoppar över raden med n=0 element
     for (j in 1:W) { #hoppar över kolumnen med W=0
       if (x$w[i] <= j) {
-        m[i+1,j+1] <- max(m[i,j+1],(m[i,j-x$w[i]+1]+x$v[i])) 
+        m[i+1,j+1] <- max(m[i,j+1],(m[i,j-x$w[i]+1]+x$v[i]))
+        if (m[i,j+1]<(m[i,j-x$w[i]+1]+x$v[i]) && !i%in%elements) {
+          elements<-append(elements,i)
+        }
       } else {
         m[i+1,j+1] <- m[i, j+1]
       }
     }
   }
- return(m[nrow(m),ncol(m)])
+ return(list(value=round(m[nrow(m),ncol(m)]), elements=elements))
 }
-
-
