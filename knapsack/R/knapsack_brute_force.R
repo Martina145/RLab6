acb7c6bc1c39170e456c99bc75df3dbbef218732
Martina Sandberg <Martini@c-213-180.eduroam.liu.se>
2015-10-02 14:26:29 +0200

    Brute force function updated

--- knapsack/R/knapsack_brute_force.R
+++ knapsack/R/knapsack_brute_force.R
@@ -1,8 +1,7 @@
 set.seed(42)
-n <- 5
+n <- 2000
 knapsack_objects <-
   data.frame(
-    item=c(1:n),
     w=sample(1:4000, size = n, replace = TRUE),
     v=runif(n = n, 0, 10000)
   )
@@ -24,7 +23,7 @@ knapsack_brute_force <- function(x,W){
       } else {
         if( K[w+1,j] >= K[w+1-wj,j]+value ) {
           K[w+1,j+1]<-K[w+1,j]
-          matrix_item[w+1,j+1]<-matrix_item[w+1,j]      		
+          matrix_item[w+1,j+1]<-matrix_item[w+1,j]          
         } else {
           K[w+1,j+1]<-K[w+1-wj,j]+value
           matrix_item[w+1,j+1]<-item
@@ -32,25 +31,25 @@ knapsack_brute_force <- function(x,W){
       }
     }
   }
-  
-  knapsack <- list(K=K,Item=matrix_item)
-  W <- dim(knapsack$K)[1]
+  W <- dim(K)[1]
   itens <- c()
-  col <- dim(knapsack$K)[2]
-  selected_item <- knapsack$Item[W,col]
+  col <- dim(K)[2]
+  selected_item <- matrix_item[W,col]
   while(selected_item!=0) {
-    selected_item<-knapsack$Item[W,col]
+    selected_item<-matrix_item[W,col]
     if(selected_item!=0) {
-      selected_item_value<-x[x$item == selected_item,]
-      if(-knapsack$K[W - selected_item_value$w,col-1]+knapsack$K[W,col]==selected_item_value$v) {
+      selected_item_value<-x[as.numeric(rownames(x)) == selected_item,]
+      if(-K[W - selected_item_value$w,col-1]+K[W,col]==selected_item_value$v) {
         W <- W - selected_item_value$w
         itens<-c(itens,selected_item)
       }
       col <- col - 1
     }
   }
-  
-  return(itens)
+  return(list(value=round(sum(x[itens,]$v)), elements=sort(itens)))
 }
 
-knapsack_brute_force(knapsack_objects, 3000)
+
+#knapsack_brute_force(x = knapsack_objects[1:8,], W = 3500)
+#knapsack_brute_force(x = knapsack_objects[1:12,], W = 2000)
+
